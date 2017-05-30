56674b49b1f0df9e15ef9d86083a2e3c8b50eb8e
Ícaro Heimig <icaro.heimig@corp.globo.com>
2017-05-30 19:44:08 -0300

    feat: add svg icons to storybook and loader

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
+
 import MediaItem from '../src/components/mediaItem';
+import Spinner from '../src/components/spinner';
+import SearchIcon from '../src/components/SearchIcon';
 
 storiesOf('MediaItem', module)
   .add('without trailer', () => (
@@ -8,4 +11,18 @@ storiesOf('MediaItem', module)
   ))
   .add('with trailer', () => (
     <MediaItem width="560px" height="315px" trailer="ONHBaC-pfsk" />
+  ));
+
+storiesOf('Loader', module)
+  .add('spinning', () => (
+    <Spinner />
+  ));
+
+
+storiesOf('SVG Icons', module)
+  .add('Search', () => (
+    <div style={{position: "relative", padding: "20px"}}>
+      <h1 className="page-title">Search Icon</h1>
+      <SearchIcon />
+    </div>
   ));
