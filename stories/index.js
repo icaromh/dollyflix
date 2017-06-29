7ba4940cbb5e5dbb16e118045db70fbdfc951bc4
Ícaro Heimig <icaro.mh@gmail.com>
2017-06-28 23:05:48 -0300

    Feat/storybook (#10)
    * feat: add storybook
    
    * feat: add svg icons to storybook and loader
    
    * fix: change @kadira from @storybook/react
    
    * feat: add MediaItem to storybook

---
+++ stories/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@storybook/react';
+
+import MediaItem from '../src/components/mediaItem';
+import Spinner from '../src/components/spinner';
+import SearchIcon from '../src/components/SearchIcon';
+import SerieItem from '../src/components/serieItem';
+
+import SerieList from '../src/containers/serieList';
+
+import series from './fixtures/series';
+
+storiesOf('SerieItem', module)
+  .add('all', () => (
+    <SerieItem
+      serie={series[1]}
+      onClick={() => console.log('click')}
+      />))
+
+storiesOf('Loader', module)
+  .add('spinning', () => (
+    <Spinner />
+  ));
+
+
+storiesOf('SVG Icons', module)
+  .add('Search', () => (
+    <div style={{position: "relative", padding: "20px"}}>
+      <h1 className="page-title">Search Icon</h1>
+      <SearchIcon />
+    </div>
+  ));
