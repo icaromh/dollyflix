7c8d2c94ef2d2303c6602c872325fc45ba7c2f92
Icaro Heimig <icaro.mh@gmail.com>
2017-11-21 00:58:28 -0200

    feat: sort seasons correctly

--- src/reducers/reducer_seasons.js
+++ src/reducers/reducer_seasons.js
@@ -1,16 +1,18 @@
 import { GET_SHOW, SELECT_SHOW } from '../actions/index';
 
+const sortCriteria = (a,b) => a - b;
+
 export default function(state = [], action) {
   switch (action.type) {
     case SELECT_SHOW:
     case GET_SHOW:
       const data = action.payload.data || action.payload
       return data.episodes
-        .map(ep => ep.season)
+        .map(ep => parseInt(ep.season, 10))
         .reduce((acc, el) => {
           return acc.indexOf(el) === -1 ? acc.concat(el) : acc;
         }, [])
-        .sort();
+        .sort(sortCriteria);
     default:
       return state;
   }
