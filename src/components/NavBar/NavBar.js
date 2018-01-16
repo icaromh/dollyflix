79ce5ad0220f4e0a676f83d2cbb07e070c870118
Ícaro Heimig <icaro.mh@gmail.com>
2018-01-16 14:14:58 -0200

    feat: add GA event to favorites route click

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router'
+import PropTypes from 'prop-types'
 import ReactGA from 'react-ga'
 
 import SearchBar from '../../containers/SearchBar'
 import {
   EVENT_CATEGORY_NAVIGATION,
+  NAVIGATION_FAVORITE_CLICK,
   NAVIGATION_HOME_CLICK,
 } from '../../constants'
 
 const NavBar = ({ location }) => {
-  const handleOnClickNav = () => {
+  const handleOnClickNav = (action) => {
     ReactGA.event({
       category: EVENT_CATEGORY_NAVIGATION,
-      action: NAVIGATION_HOME_CLICK,
+      action,
     })
   }
 
@@ -25,7 +26,7 @@ const NavBar = ({ location }) => {
         <div className="row">
           <div className="col-md-6 col-xs-5">
             <div className="navbar-header">
-              <Link to="/" className="navbar-brand" onClick={() => handleOnClickNav()}>
+              <Link to="/" className="navbar-brand" onClick={() => handleOnClickNav(NAVIGATION_HOME_CLICK)}>
                 <img alt="Brand" className="navbar-brand-image" src="/images/dollyflix.png" height="40px" />
                 <span className="hidden-xs">DollyFlix</span>
               </Link>
@@ -33,7 +34,7 @@ const NavBar = ({ location }) => {
 
             <ul className="nav navbar-nav">
               <li className={isActive('/favorites')}>
-                <Link to="/favorites" onClick={() => handleOnClickNav()}>
+                <Link to="/favorites" onClick={() => handleOnClickNav(NAVIGATION_FAVORITE_CLICK)}>
                   Favoritos
                 </Link>
               </li>
